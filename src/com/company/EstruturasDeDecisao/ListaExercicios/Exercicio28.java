ebc0adae6b4fdd3e839236e12771d117eabc0a7a
SoulsHunter-c <caiocotta2004@gmail.com>
2022-03-29 23:39:06 -0300

    .

--- src/com/company/EstruturasDeDecisao/ListaExercicios/Exercicio28.java
+++ src/com/company/EstruturasDeDecisao/ListaExercicios/Exercicio28.java
@@ -1,11 +1,16 @@
 package com.company.EstruturasDeDecisao.ListaExercicios;
 import java.util.Scanner;
 public class Exercicio28 {
-    private static final double FILE_MENOS_QUE_5 = 4.90;
-    private static final double FILE_MAIOR_QUE_5 = 5.80;
-    private static final double ALCATRA_MENOS_QUE_5 = 4.90;
+    private static final double FILE_ATE_5KG = 4.90;
+    private static final double FILE_MAIOR_QUE_5KG = 5.80;
+    private static final double ALCATRA_ATE_5KG = 4.90;
+    private static final double ALCATRA_MAIOR_QUE_5KG = 6.80;
+    private static final double PICANHA_ATE_5KG = 6.90;
+    private static final double PICANHA_MAIS_QUE_5KG = 7.80;
+    private static final double DESCONTO_PARA_CARTAO = 0.05;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double totalDaCompra = 0;
 
 
         System.out.println("                     ATÉ 5 KG            ACIMA DE 5KG");
@@ -18,5 +23,33 @@ public class Exercicio28 {
         double quilosDesejados = scanner.nextByte();
         System.out.println("Escolha a forma de pagamento:\n(1)-CARTAO TABAJARA\n(2)-OUTRO");
         byte formaDePagamento = scanner.nextByte();
+        if(quilosDesejados > 5){
+            switch (carneEscolhida) {
+                case 1 -> totalDaCompra = quilosDesejados * FILE_MAIOR_QUE_5KG;
+                case 2 -> totalDaCompra = quilosDesejados * ALCATRA_MAIOR_QUE_5KG;
+                case 3 -> totalDaCompra = quilosDesejados * PICANHA_MAIS_QUE_5KG;
+            }
+        }else if(quilosDesejados < 5){
+            totalDaCompra = switch (carneEscolhida) {
+                case 1 -> quilosDesejados * FILE_ATE_5KG;
+                case 2 -> quilosDesejados * ALCATRA_ATE_5KG;
+                case 3 -> quilosDesejados * PICANHA_ATE_5KG;
+                default -> totalDaCompra;
+            };
+        }
+        if(formaDePagamento == 1){
+            System.out.println("CARNE ESCOLHIDA:%.2f " + carneEscolhida);
+            System.out.println("QUANTIDADE ESCOLHIDA: " + quilosDesejados);
+            System.out.println("PREÇO SEM DESCONTO: " + totalDaCompra);
+            System.out.printf("PREÇO COM DESCONTO: %.2f",calculaDesconto(totalDaCompra));
+        }else{
+            System.out.println("CARNE ESCOLHIDA: " + carneEscolhida);
+            System.out.println("QUANTIDADE ESCOLHIDA: " + quilosDesejados);
+            System.out.println("PREÇO SEM DESCONTO: " + totalDaCompra);
+            System.out.printf("PREÇO COM DESCONTO: %.2f" , calculaDesconto(totalDaCompra));
+        }
+    }
+    private static double calculaDesconto(double totalDaCompra){
+        return totalDaCompra - (totalDaCompra * DESCONTO_PARA_CARTAO);
     }
 }
