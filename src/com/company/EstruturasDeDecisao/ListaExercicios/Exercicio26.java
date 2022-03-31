9d01bccdace766019afc6680a31ee2249cd74791
Soulshunter-c <caiocotta2004@gmail.com>
2022-03-31 17:03:42 -0300

    .

--- src/com/company/EstruturasDeDecisao/ListaExercicios/Exercicio26.java
+++ src/com/company/EstruturasDeDecisao/ListaExercicios/Exercicio26.java
@@ -2,6 +2,6 @@ package com.company.EstruturasDeDecisao.ListaExercicios;
 import java.util.Scanner;
 public class Exercicio26 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
     }
 }
