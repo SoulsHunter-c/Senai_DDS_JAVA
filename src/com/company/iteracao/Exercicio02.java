8f4286346be39e88783b39379e152bc7a7bd9b1a
Soulshunter-c <caiocotta2004@gmail.com>
2022-04-07 17:23:44 -0300

    Soulshunter-c

--- src/com/company/iteracao/Exercicio02.java
+++ src/com/company/iteracao/Exercicio02.java
@@ -5,7 +5,7 @@ public class Exercicio02 {
     public static void main(String[] args) {
         Integer[] numeros = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
         Integer i = 0;
-        while (numeros[i] < 20) {
+        while (numeros[i] < numeros.length) {
             boolean eDivisivelPeloDivisor = numeros[i] % DIVISOR == 0;
             if (eDivisivelPeloDivisor) {
                 System.out.println("O numero " + numeros[i] + " é divisivel por 3");
