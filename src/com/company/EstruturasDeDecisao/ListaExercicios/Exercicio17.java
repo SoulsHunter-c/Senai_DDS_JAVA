396a9e435b68e7408c28bcab3624363b3c16a5e5
SoulsHunter-c <caiocotta2004@gmail.com>
2022-04-07 22:25:52 -0300

    .

---
+++ src/com/company/EstruturasDeDecisao/ListaExercicios/Exercicio17.java
@@ -0,0 +1,19 @@
+package com.company.EstruturasDeDecisao.ListaExercicios;
+
+import java.util.Scanner;
+
+public class Exercicio17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("digite um ano: ");
+        int anoInformado = scanner.nextInt();
+        boolean anoBissexto = anoInformado % 4 == 0 && anoInformado % 100 != 0 || anoInformado % 400 == 0;
+        if (anoBissexto) {
+            System.out.println("O ano é bissexto");
+        } else {
+            System.out.println("O ano não é bissexto");
+        }
+        scanner.close();
+    }
+}
