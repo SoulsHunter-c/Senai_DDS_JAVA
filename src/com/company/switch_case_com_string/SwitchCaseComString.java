2f1bd483699d5e184f79d3f80957eb70c5e0cc25
Soulshunter-c <caiocotta2004@gmail.com>
2022-06-21 16:18:38 -0300

    .

---
+++ src/com/company/switch_case_com_string/SwitchCaseComString.java
@@ -0,0 +1,20 @@
+package com.company.switch_case_com_string;
+
+public class SwitchCaseComString {
+
+    public static void main(String[] args) {
+        String carro = args.length > 0 ? args[0] : "";
+        /*
+        if (carro.equals("Audi")){
+            System.out.println("Alemao");
+        }else if (carro.equals("Ferrari")){
+            System.out.println("Italiano");
+        }*/
+        switch (carro) {
+            case "Audi" -> System.out.println("Alemao");
+            case "Ferrari" -> System.out.println("Italiano");
+            case "Honda" -> System.out.println("Japones");
+            default -> System.out.println("Não informou nenhum carro!");
+        }
+    }
+}
